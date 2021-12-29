a80e81ba52ee01a04be92752af59ef3f23cdf7ce
Malik-62 <67704241+Malik-62@users.noreply.github.com>
2021-12-29 11:15:18 +0100

    Initial Commit

---
+++ Shindys_Liste/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shindys_Liste
+//
+//  Created by Malik Amajjoud on 29.12.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shindys_Liste/Shindys_ListeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shindys_ListeApp.swift
+//  Shindys_Liste
+//
+//  Created by Malik Amajjoud on 29.12.21.
+//
+
+import SwiftUI
+
+@main
+struct Shindys_ListeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
