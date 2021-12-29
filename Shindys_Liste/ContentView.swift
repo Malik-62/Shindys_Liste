d510eec8cec5acda35168dbbe9524991055347b4
Malik-62 <67704241+Malik-62@users.noreply.github.com>
2021-12-29 18:01:15 +0100

    ForEach schmeißt nen Fatal error wenn wir den Index ändern, Suchleiste ist ein Dummy.  Man kann keine Objekte erstellen, aber es sieht schick aus.

--- Shindys_Liste/ContentView.swift
+++ Shindys_Liste/ContentView.swift
@@ -8,24 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var selected = false
+    // @State var blau = [Item]()
+    // @State var rot = [Item]()
+    @State var neuerName = ""
+    @State var Schaukel = true
+    @State var blau = [Item(Name: "Eier"),Item(Name: "Milch"),Item(Name: "Mehl")]
+    @State var rot = [Item(Name: "Tomaten"),Item(Name: "Gurken"),Item(Name: "Paprika")]
+    
     var body: some View {
-        List {
-            Section(header: Text("zu kaufen")){
-                ForEach(1..<6) { i in
-                                Text("Hi")
+        
+        VStack{
+            HStack{
+                SearchBar(text: $neuerName)
+                Image(systemName: "plus.circle.fill")
+            }.padding()
+            
+            if Schaukel {
+                List {
+                    Section(header: Text("einkaufen")){
+                        ForEach(0..<blau.count) { i in
+                            Button(blau[i].name) {
+                                rot.append(blau[i])
+                                blau.remove(at: i)
                             }
+                            
+                        }
+                    }.tint(.green)
+                    
+                    Section(header: Text("erledigt")){
+                        ForEach(0..<rot.count) { i in
+                            Button(rot[i].name) {
+                                blau.append(rot[i])
+                                rot.remove(at: i)
+                            }.tint(.red)
+                        }
+                    }
+                }
             }
             
-            Section(header: Text("erledigt")){
-                Text("Hi")//.bold()
-                Text("Hi")
-                Text("Hi")
-            }
-           
         }
-         
-
     }
 }
 
